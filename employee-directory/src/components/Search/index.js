2183c907dc0e25bc6ba3036cb657e08ef03269f0
Kathleen Kelly <murphy.kaite@gmail.com>
2020-08-04 01:51:38 -0700

    added searchform component

---
+++ employee-directory/src/components/Search/index.js
@@ -0,0 +1,32 @@
+import React from "react";
+import "./style.css";
+
+function SearchForm(props) {
+    return (
+        <form className="search">
+            <div className="form-group">
+                <label htmlFor="employeeName">Employee Name: </label>
+                <input
+                value={props.search}
+                onChange={props.handleInputChange}
+                name="employee"
+                list="employees"
+                type="text"
+                className="form-control"
+                placeholder="Type in an employee name"
+                id="employee"
+                />
+                <datalist id="employees">
+                    {props.employees.map(employee => (
+                        <option value={employee} key={employee} />
+                    ))}
+                </datalist>
+                <button type="submit" onClick={props.handleFormSubmit} className="btn btn-success">
+                    Search
+                </button>
+            </div>
+        </form>
+    )
+}
+
+export default SearchForm;
