89496d072ff5381348c6b714915ccd74bb300895
Kathleen Kelly <murphy.kaite@gmail.com>
2020-08-04 01:21:01 -0700

    added app render code

--- employee-directory/src/App.js
+++ employee-directory/src/App.js
@@ -1,15 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Employee from "./components/Employee/Employee";
+
 import Directory from "./src/components/Directory.js"
 import Search from "./components/Search/Search";
-import Employee from "./components/Employee/Employee";
 import Navbar from "./components/Footer/Footer";
 import Footer from "./components/Footer/Footer";
 import List from "./components/List/List"
 
 // http://dummy.restapiexample.com/api/v1/employees
 
+
+class App extends Component {
+  state = {
+    employees
+  };
+
+  removeEmployee = id => {
+    const employees = this.state.friends.filter(friend => friend.id !== id);
+
+    this.setState({ employees });
+  };
+
+  render() {
+    return (
+      <Wrapper>
+        <Title>Employee Directory</Title>
+        {this.state.employees.map(employee => (
+          <Employee
+          removeEmployee={this.removeEmployee}
+          id={employee.id}
+          key={employee.id}
+          name={employee.name}
+          email={employee.email}
+          phone={employee.phone}
+          />
+        ))}
+      </Wrapper>
+    )
+  }
+
+}
 
 function App() {
   return (
